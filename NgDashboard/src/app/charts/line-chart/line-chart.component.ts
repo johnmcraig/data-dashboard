fc109bc7613330b302c1f3f3aa2f43a8161d3795
johnmcraig <jmcraig32884@gmail.com>
2018-07-02 23:51:54 -0400

    Created line chart in app with mock data

--- NgDashboard/src/app/charts/line-chart/line-chart.component.ts
+++ NgDashboard/src/app/charts/line-chart/line-chart.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
 
 const LINE_CHART_SAMPLE_DATA: any[] = [
-  { data: [], label: 'Sentiment Analysis'},
-  { data: [], label: 'Image Recognition'},
-  { data: [], label: 'Forecasting'}
+  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+  { data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
+  { data: [52, 34, 49, 53, 68, 62], label: 'Forecasting'}
 ];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -14,6 +17,15 @@ const LINE_CHART_SAMPLE_DATA: any[] = [
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartType = 'line';
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend = true;
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
