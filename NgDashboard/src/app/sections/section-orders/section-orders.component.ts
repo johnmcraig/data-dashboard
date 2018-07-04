cb5ea3049d7af1bf86841d66dc31555d7663589f
johnmcraig <jmcraig32884@gmail.com>
2018-07-04 17:09:58 -0400

    Added models and mock data

--- NgDashboard/src/app/sections/section-orders/section-orders.component.ts
+++ NgDashboard/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -6,10 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-orders.component.css']
 })
 export class SectionOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainstbakery@eample.com'
+      },
+      total: 230,
+      placed: new Date(2018, 1, 1),
+      fulfilled: new Date(2018, 1, 5),
+      status: 'Comleted'
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainstbakery@eample.com'
+      },
+      total: 230,
+      placed: new Date(2018, 1, 1),
+      fulfilled: new Date(2018, 1, 5),
+      status: 'Comleted'
+    },
+    {
+      id: 3,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainstbakery@eample.com'
+      },
+      total: 230,
+      placed: new Date(2018, 1, 1),
+      fulfilled: new Date(2018, 1, 5),
+      status: 'Comleted'
+    },
+    {
+      id: 4,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainstbakery@eample.com'
+      },
+      total: 230,
+      placed: new Date(2018, 1, 1),
+      fulfilled: new Date(2018, 1, 5),
+      status: 'Comleted'
+    },
+    {
+      id: 5,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainstbakery@eample.com'
+      },
+      total: 230,
+      placed: new Date(2018, 1, 1),
+      fulfilled: new Date(2018, 1, 5),
+      status: 'Comleted'
+    }
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
