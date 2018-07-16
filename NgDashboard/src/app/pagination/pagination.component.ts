684994e41800c0442368f1981a43cd7ab06bcbb1
johnmcraig <jmcraig32884@gmail.com>
2018-07-16 01:02:54 -0400

    Set min and max pages to load in orders

--- NgDashboard/src/app/pagination/pagination.component.ts
+++ NgDashboard/src/app/pagination/pagination.component.ts
@@ -10,7 +10,7 @@ export class PaginationComponent implements OnInit {
   @Input() page: number;
   @Input() count: number;
   @Input() perPage: number;
-  @Input() pageToShow: number;
+  @Input() pagesToShow: number;
   @Input() loading: boolean;
 
   @Output() goPrev = new EventEmitter<boolean>();
@@ -40,5 +40,43 @@ export class PaginationComponent implements OnInit {
 
   isLastPage(): boolean {
     return this.perPage * this.page >= this.count;
+  }
+
+  getMin(): number {
+    return ((this.perPage * this.page) - this.perPage) + 1;
+  }
+
+  getMax(): number {
+    let max = this.perPage * this.page;
+    if (max > this.count) {
+      max = this.count;
+    }
+    return max;
+  }
+
+  getPages(): number[] {
+    const totalPages = Math.ceil(this.count / this.perPage);
+    const thisPage = this.page || 1;
+    const pagesToShow = this.pagesToShow || 9;
+    const pages: number[] = [];
+    pages.push(thisPage);
+    // console.log('Starting llop wiht: total pages: ', totalPages, 'thisPage:', thisPage, 'pagesToShow', pagesToShow);
+    for (let i = 0; i < pagesToShow - 1; i++) {
+      // console.log('pages[]:', pages);
+      if (pages.length < pagesToShow) {
+        if (Math.min.apply(null, pages) > 1) {
+          pages.push(Math.min.apply(null, pages) - 1);
+          // console.log('pushing ', Math.min.apply(null, psges) - 1, 'onto array');
+        }
+      }
+      if (pages.length < pagesToShow) {
+        if (Math.max.apply(null, pages) < totalPages) {
+          pages.push(Math.max.apply(null, pages) + 1);
+           // console.log('pushing ', Math.max.apply(null, psges) + 1, 'onto array');
+        }
+      }
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
   }
 }
--- NgDashboard/src/app/services/sales-data.service.ts
+++ NgDashboard/src/app/services/sales-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class SalesDataService {
 constructor(private _http: Http) { }
 
 getOrders(pageIndex: number, pageSize: number) {
-  return this._http.get('http://localhost:5000/api/order' + pageIndex + '/' + pageSize)
+  return this._http.get('http://localhost:5001/api/order' + pageIndex + '/' + pageSize)
   .subscribe(res => res.json());
  }
 }
