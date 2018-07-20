3ad4f855828e68bb95485117cecf0b725ec1e521
johnmcraig <jmcraig32884@gmail.com>
2018-07-19 23:18:20 -0400

    Used ng6 updates to access api

--- NgDashboard/src/app/services/sales-data.service.ts
+++ NgDashboard/src/app/services/sales-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-// import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -12,12 +12,12 @@ export class SalesDataService {
 
   getOrders(pageIndex: number, pageSize: number) {
      this.http.get('http://localhost:5001/api/order' + pageIndex + '/' + pageSize)
-      .subscribe(response => { response.json(); });
+      .pipe(map(response => { response.json(); }));
   }
 
   // getOrdersByCustomer(n: number) {
   //   return this.http.get('http://locslhody:5001/api/order/bycustomer/' + n)
-  //   .subscribe(response => response.json());
+  //   .pipe(map(response => response.json()));
   // }
 
   // getOrdersByState() {
