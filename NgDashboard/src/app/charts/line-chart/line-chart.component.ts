74ea3ceb235ae45c5c984746bbdb5a31506fd40b
johnmcraig <jmcraig32884@gmail.com>
2018-08-02 13:21:34 -0400

    Setup line chart to get data from API

--- NgDashboard/src/app/charts/line-chart/line-chart.component.ts
+++ NgDashboard/src/app/charts/line-chart/line-chart.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+import { SalesDataService } from '../../services/sales-data.service';
 
-const LINE_CHART_SAMPLE_DATA: any[] = [
-  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
-  { data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
-  { data: [52, 34, 49, 53, 68, 62], label: 'Forecasting'}
-];
+// const LINE_CHART_SAMPLE_DATA: any[] = [
+//   { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+//   { data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
+//   { data: [52, 34, 49, 53, 68, 62], label: 'Forecasting'}
+// ];
 
-const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+// const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -16,10 +17,13 @@ const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 })
 export class LineChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesDataService: SalesDataService) { }
 
-  lineChartData = LINE_CHART_SAMPLE_DATA;
-  lineChartLabels = LINE_CHART_LABELS;
+  topCustomers: string[];
+  allOrders: any[];
+
+  lineChartData: any; // LINE_CHART_SAMPLE_DATA;
+  lineChartLabels: any; // LINE_CHART_LABELS;
   lineChartType = 'line';
   lineChartOptions: any = {
     responsive: true
@@ -28,6 +32,8 @@ export class LineChartComponent implements OnInit {
   lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
+    this._salesDataService.getOrders(1, 100).subscribe(response => {
+      console.log('getOrders', response);
+    });
   }
-
 }
