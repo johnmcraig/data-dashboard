509e18e3557e597df94c77fda497032dce3c30ac
johnmcraig <jmcraig32884@gmail.com>
2020-09-05 22:10:18 -0400

    updated using statements for entity classes and context class

--- src/DataDashboard.Api/Controllers/PaginatedResponse.cs
+++ src/DataDashboard.Api/Controllers/PaginatedResponse.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
-using DashboardApi.Models;
+using DataDashboard.Core.Entities;
+using DataDashboard.Infrastructure.Data;
 
 namespace DataDashboard.Api.Controllers
 {
