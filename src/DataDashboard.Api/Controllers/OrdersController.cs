8b2e6ae96a4ba27b44427d7977b2e705f5c2d759
johnmcraig <jmcraig32884@gmail.com>
2020-09-17 19:28:15 -0400

    Reordered action methods, introduced unit of work pattern athrough DI

--- src/DataDashboard.Api/Controllers/OrdersController.cs
+++ src/DataDashboard.Api/Controllers/OrdersController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using DataDashboard.Core.Entities;
+using DataDashboard.Core.Interfaces;
 using DataDashboard.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,37 +15,74 @@ namespace DataDashboard.Api.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly ApiContext _context;
+        private readonly IOrderRepository _repo;
         ILogger<OrdersController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public OrdersController(ApiContext context, ILogger<OrdersController> logger)
+        public OrdersController(ApiContext context,
+            IOrderRepository repo,
+            ILogger<OrdersController> logger,
+            IUnitOfWork unitOfWork)
         {
             _context = context;
+            _repo = repo;
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         /*
         Pagination for the server
         GET api/order/pageNumber/pageSize
          */
-        [HttpGet("{pageIndex:int}/{pageSize:int}")]
-        public IActionResult Get(int pageIndex, int pageSize)
+        [HttpGet] //("{pageIndex:int}/{pageSize:int}") int pageIndex, int pageSize
+        public async Task<IActionResult> GetAll()
         {
-            var data = _context.Orders
-                .Include(o => o.Customer)
-                .OrderByDescending(c => c.Placed);
+            _logger.LogInformation("Attempting to get all orders");
 
-            var page = new PaginatedResponse<Order>(data, pageIndex, pageSize);
-
-            var totalCount = data.Count();
-            var totalPages = Math.Ceiling((double)totalCount / pageSize);
-
-            var response = new
+            try
             {
-                Page = page,
-                TotalPages = totalPages
-            };
+                var data = await _unitOfWork.Orders.ListAllAsync();
 
-            return Ok(response);
+                _logger.LogInformation("Successfully retrived all records");
+
+                return Ok(data);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError($"There was an error: {ex.Message}");
+                
+                return BadRequest(ex.Message);
+            }
+            
+            // var data = _context.Orders
+            //     .Include(o => o.Customer)
+            //     .OrderByDescending(c => c.Placed);
+
+            // var page = new PaginatedResponse<Order>(data, pageIndex, pageSize);
+
+            // var totalCount = data.Count();
+            // var totalPages = Math.Ceiling((double)totalCount / pageSize);
+
+            // var response = new
+            // {
+            //     Page = page,
+            //     TotalPages = totalPages
+            // };
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            // var order = _context.Orders.Include(o => o.Customer)
+            //     .First(o => o.Id == id);
+
+            var order = await _unitOfWork.Orders.GetByIdAsync(id);
+
+            if (order == null)
+                return NotFound();
+
+            return Ok(order);
         }
 
         [HttpGet("ByState")]
@@ -74,20 +113,11 @@ namespace DataDashboard.Api.Controllers
                 {
                     Name = _context.Customers.Find(grp.Key).Name,
                         Total = grp.Sum(x => x.Total)
-                }).OrderByDescending(res => res.Total) //descending number of Customers in order
+                }).OrderByDescending(res => res.Total)
                 .Take(n)
                 .ToList();
 
             return Ok(groupResult);
-        }
-
-        [HttpGet("GetOrder/{id}", Name = "GetOrder")]
-        public IActionResult GetOrder(int id)
-        {
-            var order = _context.Orders.Include(o => o.Customer)
-                .First(o => o.Id == id);
-
-            return Ok(order);
         }
 
     }
