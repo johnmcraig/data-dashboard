19c18a829dd38b274e7dbf4646f49fccdbbf8279
johnmcraig <jmcraig32884@gmail.com>
2020-09-17 19:27:34 -0400

    Refactored to use unit of work pattern with DI

--- src/DataDashboard.Api/Controllers/CustomersController.cs
+++ src/DataDashboard.Api/Controllers/CustomersController.cs
@@ -4,6 +4,8 @@ using DataDashboard.Infrastructure.Data;
 using DataDashboard.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using DataDashboard.Core.Interfaces;
 
 namespace DataDashboard.Api.Controllers
 {
@@ -13,25 +15,36 @@ namespace DataDashboard.Api.Controllers
     {
         private readonly ApiContext _context;
         ILogger<CustomersController> _logger;
+        private readonly ICustomerRepository _repo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CustomersController(ApiContext context, ILogger<CustomersController> logger)
+        public CustomersController(ApiContext context, 
+        ILogger<CustomersController> logger,
+        ICustomerRepository repo,
+        IUnitOfWork unitOfWork)
         {
             _context = context;
             _logger = logger;
+            _repo = repo;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> GetAll()
         {
-            var data = _context.Customers.OrderBy(c => c.Id);
+           // var data = _context.Customers.OrderBy(c => c.Id);
 
-            return Ok(data);
+            var customers = await _unitOfWork.Customers.ListAllAsync();
+
+            return Ok(customers);
         }
 
-        [HttpGet("{id}", Name = "GetCustomer")]
-        public IActionResult Get(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
         {
-            var customer = _context.Customers.Find(id);
+           // var customer = _context.Customers.Find(id);
+
+           var customer = await _unitOfWork.Customers.GetByIdAsync(id);
 
             return Ok(customer);
         }
