e40e59e43e787af96ee46be84c5b47dcf5b9c268
johnmcraig <jmcraig32884@gmail.com>
2020-10-01 00:02:28 -0400

    Refactored string queries into proper escaped format

--- src/DataDashboard.Infrastructure/Data/CustomerRepository.cs
+++ src/DataDashboard.Infrastructure/Data/CustomerRepository.cs
@@ -12,6 +12,7 @@ namespace DataDashboard.Infrastructure.Data
     {
         private readonly ISqlDataAccess _dataAccess;
         private const string ConnectionString = "default";
+
         public CustomerRepository(ISqlDataAccess dataAccess)
         {
             _dataAccess = dataAccess;
@@ -19,11 +20,12 @@ namespace DataDashboard.Infrastructure.Data
 
         public async Task<Customer> GetByIdAsync(int id)
         {
-            var query = @"SELECT * FROM ""public"".""Customers"" WHERE ""Id"" = @Id";
+            var query = "SELECT * FROM \"public\".\"Customers\" WHERE \"Id\" = @Id";
 
             try
             {
-                var customer = await _dataAccess.LoadData<Customer, dynamic>(query, new
+                var customer = await _dataAccess.LoadData<Customer, dynamic>
+                    (query, new
                     {
                         Id = id
                     }, ConnectionString);
@@ -31,24 +33,23 @@ namespace DataDashboard.Infrastructure.Data
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
 
         public async Task<IList<Customer>> ListAllAsync()
         {
-            var query = @"SELECT * FROM ""public"".""Customers""";
+            var query = "SELECT * FROM \"public\".\"Customers\"";
 
             try
             {
-                var customers = await _dataAccess.LoadData<Customer, dynamic>(query, new
+                var customers = await _dataAccess.LoadData<Customer, dynamic>
+                    (query, new
                     { }, ConnectionString);
                 return customers.ToList();
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
