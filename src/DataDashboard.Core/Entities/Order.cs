65ba788650d5084a016bd43c155ff479e2f782ec
johnmcraig <jmcraig32884@gmail.com>
2020-10-04 20:30:11 -0400

    Added CustomerId property for navigation

--- src/DataDashboard.Core/Entities/Order.cs
+++ src/DataDashboard.Core/Entities/Order.cs
@@ -5,6 +5,7 @@ namespace DataDashboard.Core.Entities
     public class Order : BaseEntity
     {
         public Customer Customer { get; set; }
+        public int CustomerId { get; set; }
         public decimal Total { get; set; }
         public DateTime Placed { get; set; }
         public DateTime? Completed { get; set; }
