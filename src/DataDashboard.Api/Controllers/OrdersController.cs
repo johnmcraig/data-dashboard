84a63e1a0f62bfccc3f861c5f369985443b34ac5
johnmcraig <jmcraig32884@gmail.com>
2020-10-04 20:28:32 -0400

    Added Status code responses to action methods, custom logging and error checking.

--- src/DataDashboard.Api/Controllers/OrdersController.cs
+++ src/DataDashboard.Api/Controllers/OrdersController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using DataDashboard.Core.Entities;
 using DataDashboard.Core.Interfaces;
 using DataDashboard.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -14,111 +15,62 @@ namespace DataDashboard.Api.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
-        private readonly ApiContext _context;
-        private readonly IOrderRepository _repo;
         ILogger<OrdersController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public OrdersController(ApiContext context,
-            IOrderRepository repo,
-            ILogger<OrdersController> logger,
+        public OrdersController(ILogger<OrdersController> logger,
             IUnitOfWork unitOfWork)
         {
-            _context = context;
-            _repo = repo;
             _logger = logger;
             _unitOfWork = unitOfWork;
         }
 
-        /*
-        Pagination for the server
-        GET api/order/pageNumber/pageSize
-         */
-        [HttpGet] //("{pageIndex:int}/{pageSize:int}") int pageIndex, int pageSize
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAll()
         {
             _logger.LogInformation("Attempting to get all orders");
 
             try
             {
-                var data = await _unitOfWork.Orders.ListAllAsync();
+                var orders = await _unitOfWork.Orders.ListAllAsync();
 
-                _logger.LogInformation("Successfully retrived all records");
+                _logger.LogInformation("Successfully retreived all records");
 
-                return Ok(data);
+                return Ok(orders);
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError($"There was an error: {ex.Message}");
                 
                 return BadRequest(ex.Message);
             }
-            
-            // var data = _context.Orders
-            //     .Include(o => o.Customer)
-            //     .OrderByDescending(c => c.Placed);
-
-            // var page = new PaginatedResponse<Order>(data, pageIndex, pageSize);
-
-            // var totalCount = data.Count();
-            // var totalPages = Math.Ceiling((double)totalCount / pageSize);
-
-            // var response = new
-            // {
-            //     Page = page,
-            //     TotalPages = totalPages
-            // };
-
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById(int id)
         {
-            // var order = _context.Orders.Include(o => o.Customer)
-            //     .First(o => o.Id == id);
+            _logger.LogInformation("Attempting to get a single order record by Id");
 
-            var order = await _unitOfWork.Orders.GetByIdAsync(id);
+            try
+            {
+                var order = await _unitOfWork.Orders.GetByIdAsync(id);
 
-            if (order == null)
-                return NotFound();
+                if (order == null)
+                    return NotFound();
 
-            return Ok(order);
+                _logger.LogInformation($"Successfully found record of Id: {id}");
+
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"There was an error trying to get a record by Id: {id} - {ex.Message}");
+                return BadRequest();
+            } 
         }
-
-        [HttpGet("ByState")]
-        public IActionResult ByState()
-        {
-            var orders = _context.Orders.Include(o => o.Customer).ToList();
-
-            var groupResult = orders.GroupBy(o => o.Customer.State)
-                .ToList()
-                .Select(grp => new
-                {
-                    State = grp.Key,
-                        Total = grp.Sum(x => x.Total)
-                }).OrderByDescending(res => res.Total) //descending number of States in order
-                .ToList();
-
-            return Ok(groupResult);
-        }
-
-        [HttpGet("ByCustomer")]
-        public IActionResult ByCustomer(int n)
-        {
-            var orders = _context.Orders.Include(o => o.Customer).ToList();
-
-            var groupResult = orders.GroupBy(o => o.Customer.State)
-                .ToList()
-                .Select(grp => new
-                {
-                    Name = _context.Customers.Find(grp.Key).Name,
-                        Total = grp.Sum(x => x.Total)
-                }).OrderByDescending(res => res.Total)
-                .Take(n)
-                .ToList();
-
-            return Ok(groupResult);
-        }
-
     }
 }
