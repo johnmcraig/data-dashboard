caa549d2723052c602d823f9a4f2241d641bd8e9
johnmcraig <jmcraig32884@gmail.com>
2020-10-04 20:27:04 -0400

    Added Status code responses to action methods. Refactored methods to include logging and check for errors.

--- src/DataDashboard.Api/Controllers/CustomersController.cs
+++ src/DataDashboard.Api/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using DataDashboard.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace DataDashboard.Api.Controllers
 {
@@ -15,37 +16,62 @@ namespace DataDashboard.Api.Controllers
     {
         private readonly ApiContext _context;
         ILogger<CustomersController> _logger;
-        private readonly ICustomerRepository _repo;
         private readonly IUnitOfWork _unitOfWork;
 
-        public CustomersController(ApiContext context, 
-        ILogger<CustomersController> logger,
-        ICustomerRepository repo,
+        public CustomersController(ILogger<CustomersController> logger,
         IUnitOfWork unitOfWork)
         {
-            _context = context;
             _logger = logger;
-            _repo = repo;
             _unitOfWork = unitOfWork;
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAll()
         {
-            var customers = await _unitOfWork.Customers.ListAllAsync();
+            _logger.LogInformation($"Attempting to get all customer records");
 
-            return Ok(customers);
+            try
+            {
+                var customers = await _unitOfWork.Customers.ListAllAsync();
+
+                return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Could not find records please see the following: {ex.Message}");
+                return BadRequest();
+            }
+            
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById(int id)
         {
-           var customer = await _unitOfWork.Customers.GetByIdAsync(id);
+            _logger.LogInformation($"Attempting to get a singe customer record");
+            try
+            {
+                var customer = await _unitOfWork.Customers.GetByIdAsync(id);
+                
+                if (customer == null)
+                    return NotFound();
 
-            return Ok(customer);
+                return Ok(customer);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Could not find the record with Id: {id}. Please see the following: {ex.Message}");
+                return BadRequest();
+            } 
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult Create([FromBody] Customer customer)
         {
             if (customer == null)
