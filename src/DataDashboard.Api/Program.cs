9e916bd3342f9eca597c604c3067d082354fca1a
johnmcraig <jmcraig32884@gmail.com>
2020-10-04 20:25:53 -0400

    Refactored Program class to create migration upon startup of App

--- src/DataDashboard.Api/Program.cs
+++ src/DataDashboard.Api/Program.cs
@@ -8,14 +8,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using DataDashboard.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using DataDashboard.Infrastructure.SeedData;
 
 namespace DataDashboard.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<ApiContext>();
+                    await dbContext.Database.MigrateAsync();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
