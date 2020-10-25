926db6d1ee3acf197347938d3e5481b069f64007
johnmcraig <jmcraig32884@gmail.com>
2020-10-25 18:49:17 -0400

    Added abstract method for searching

--- src/DataDashboard.Core/Interfaces/ICustomerRepository.cs
+++ src/DataDashboard.Core/Interfaces/ICustomerRepository.cs
@@ -1,4 +1,5 @@
 using DataDashboard.Core.Entities;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DataDashboard.Core.Interfaces
@@ -6,5 +7,6 @@ namespace DataDashboard.Core.Interfaces
     public interface ICustomerRepository : IGenericRepository<Customer>
     {
         Task<Customer> Create(Customer entity);
+        Task<IList<Customer>> FindBySearch(string search);
     }
 }
