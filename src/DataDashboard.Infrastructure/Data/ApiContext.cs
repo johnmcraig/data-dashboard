fe132f038070c8c454915179875deee762724edf
johnmcraig <jmcraig32884@gmail.com>
2020-10-28 23:58:32 -0400

    Added features when using Sqlite database to convert decimal to double and DateTime to DateTimeOffset

--- src/DataDashboard.Infrastructure/Data/ApiContext.cs
+++ src/DataDashboard.Infrastructure/Data/ApiContext.cs
@@ -1,6 +1,11 @@
 using DataDashboard.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 
 namespace DataDashboard.Infrastructure.Data
 {
@@ -14,6 +19,7 @@ namespace DataDashboard.Infrastructure.Data
             IConfiguration config) : base(options)
         {
             _config = config;
+            Database.EnsureCreated();
         }
 
         public DbSet<Customer> Customers { get; set; }
@@ -22,7 +28,37 @@ namespace DataDashboard.Infrastructure.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-             optionsBuilder.UseNpgsql(_config.GetConnectionString("default"));
+            //optionsBuilder.UseNpgsql(_config.GetConnectionString("default"));
+            optionsBuilder.UseSqlite(_config.GetConnectionString("sqlite"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in builder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties()
+                        .Where(prop => prop.PropertyType == typeof(decimal));
+                    var dateTimeProperties = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(DateTimeOffset));
+
+                    foreach (var property in properties)
+                    {
+                        builder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion<double>();
+                    }
+
+                    foreach (var property in dateTimeProperties)
+                    {
+                        builder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion(new DateTimeOffsetToBinaryConverter());
+                    }
+                }
+            }
         }
     }
 }
