059041b3dbaf663c5c054260b5956c49723ed9e0
johnmcraig <jmcraig32884@gmail.com>
2020-11-02 01:13:59 -0500

    Expanded customer hub for disconnecting

--- src/DataDashboard.Api/Hubs/CustomerHub.cs
+++ src/DataDashboard.Api/Hubs/CustomerHub.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace DataDashboard.Api.Hubs
 {
     public class CustomerHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await SendMessage();
+        }
+
         public async Task SendMessage()
         {
-            await Clients.All.SendAsync("ReceiveMessage");
+            await Clients.All.SendAsync("Message", "Successfully connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("Message", "Successfully disconnected");
+            await base.OnDisconnectedAsync(ex);
         }
     }
 }
