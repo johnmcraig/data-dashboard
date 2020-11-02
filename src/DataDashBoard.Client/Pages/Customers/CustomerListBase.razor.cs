8a057510177ffd69c48c9ac848771b6fae9df648
johnmcraig <jmcraig32884@gmail.com>
2020-11-02 01:10:53 -0500

    Created a base component class for the customer list page to inheret

---
+++ src/DataDashBoard.Client/Pages/Customers/CustomerListBase.razor.cs
@@ -0,0 +1,62 @@
+﻿using DataDashboard.Client.Contracts;
+using DataDashboard.Client.Models;
+using DataDashboard.Client.Static;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataDashboard.Client.Pages.Customers 
+{
+    public partial class CustomerListBase : IDisposable
+    {
+        private HubConnection _hubConnection;
+
+        public IList<CustomerModel> Model = new List<CustomerModel>();
+
+        [Inject]
+        public ICustomerRepository CustomerRepo { get; set; }
+
+        protected async Task OnInitializedAsync()
+        {
+            await StartHubConnection();
+            await CustomerRepo.GetAll(Endpoints.CustomersEndpoint);
+            AddDataListener();
+            //StateHasChanged();
+        }
+
+        private async Task StartHubConnection()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("/customerhub")
+                .Build();
+
+            await _hubConnection.StartAsync();
+            if (_hubConnection.State == HubConnectionState.Connected)
+            {
+                Console.WriteLine("connection started");
+            }
+        }
+
+        private void AddDataListener()
+        {
+            _hubConnection.On<IList<CustomerModel>>("", (data) =>
+            {
+                foreach (var item in data)
+                {
+                    Console.WriteLine($"Name: {item.Name}, Email: {item.Email}");
+                }
+
+                Model = data;
+                //StateHasChanged();
+            });
+        }
+
+        public void Dispose()
+        {
+            _hubConnection.DisposeAsync();
+        }
+    }
+}
