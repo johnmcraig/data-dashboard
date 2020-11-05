43e92df3abab8de704c8924b48aed970addd161a
John Craig <jmcraig32884@gmail.com>
2020-11-04 23:36:23 -0500

    Refactored interface to derive from BaseEnitiy

--- src/DataDashboard.Core/Interfaces/IGenericRepository.cs
+++ src/DataDashboard.Core/Interfaces/IGenericRepository.cs
@@ -4,7 +4,7 @@ using DataDashboard.Core.Entities;
 
 namespace DataDashboard.Core.Interfaces
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<IList<T>> ListAllAsync();
         Task<T> GetByIdAsync(int id);  
