3f6d9e694f3e8faea984e58ed38b8b8feba8339f
johnmcraig <jmcraig32884@gmail.com>
2020-11-26 12:36:43 -0500

    Added signature method to list all orders with paging

--- src/DataDashboard.Core/Interfaces/IOrderRepository.cs
+++ src/DataDashboard.Core/Interfaces/IOrderRepository.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using DataDashboard.Core.Entities;
 
 namespace DataDashboard.Core.Interfaces
 {
     public interface IOrderRepository : IGenericRepository<Order>
-    {       
+    {
+        Task<IList<Order>> ListAllWithPaging(int page, int pageSize);
     }
 }
