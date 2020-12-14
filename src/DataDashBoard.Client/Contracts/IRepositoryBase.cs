376f748d6479afe0359ed0c2a60bba6f141e15bd
johnmcraig <jmcraig32884@gmail.com>
2020-12-14 14:11:45 -0500

    Implemented Update signature method in repository interface

--- src/DataDashBoard.Client/Contracts/IRepositoryBase.cs
+++ src/DataDashBoard.Client/Contracts/IRepositoryBase.cs
@@ -8,7 +8,7 @@ namespace DataDashboard.Client.Contracts
         Task<IList<T>> GetAll(string url);
         Task<T> GetSingle(string url, int id);
         Task<T> Create(string url, T entity);
-        //Task<bool> Update(string url, T entity, int id);
+        Task<T> Update(string url, T entity, int id);
         Task Delete(string url, int id);
     }
 }
