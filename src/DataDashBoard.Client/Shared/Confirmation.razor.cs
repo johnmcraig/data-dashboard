24dc517adc902a750ee554dc50ee2ceeae19709b
johnmcraig <jmcraig32884@gmail.com>
2020-12-13 19:22:07 -0500

    Created a Confirmation modal component and razor class supporting the modal

---
+++ src/DataDashBoard.Client/Shared/Confirmation.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataDashboard.Client.Shared
+{
+    public partial class Confirmation
+    {
+        private string _modalDisplay;
+        private bool _showBackDrop;
+
+        [Parameter]
+        public string BodyMessage { get; set; }
+
+        [Parameter]
+        public EventCallback OnOKClick { get; set; }
+
+        public void Show()
+        {
+            _modalDisplay = "block;";
+            _showBackDrop = true;
+            StateHasChanged();
+        }
+
+        public void Hide()
+        {
+            _modalDisplay = "none;";
+            _showBackDrop = false;
+            StateHasChanged();
+        }
+
+    }
+}
