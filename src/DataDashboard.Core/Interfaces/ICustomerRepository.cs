7cde6b4b04dc02d3ca2fa5f44957abccd4dacaa3
johnmcraig <jmcraig32884@gmail.com>
2020-12-14 14:14:35 -0500

    Implemented Update signature method in repository interface

--- src/DataDashboard.Core/Interfaces/ICustomerRepository.cs
+++ src/DataDashboard.Core/Interfaces/ICustomerRepository.cs
@@ -10,6 +10,7 @@ namespace DataDashboard.Core.Interfaces
         Task<IList<Customer>> FindBySearch(string search);
         Task<IList<Customer>> ListAllWithPaging(int page, int pageSize);
         Task<IList<Customer>> ListAllWithSearchingAndPaging(string search, int page, int pageSize);
+        Task Update(Customer customer);
         Task Delete(int id);
     }
 }
