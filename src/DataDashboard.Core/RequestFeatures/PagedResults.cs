126e94e47d76f69ada4761b149c669042ecfe142
johnmcraig <jmcraig32884@gmail.com>
2021-01-16 00:49:00 -0500

    Created PagedResults class

---
+++ src/DataDashboard.Core/RequestFeatures/PagedResults.cs
@@ -0,0 +1,13 @@
+﻿using DataDashboard.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDashboard.Core.RequestFeatures
+{
+    public class PagedResults<T> where T : BaseEntity
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
