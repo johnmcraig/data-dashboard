efda3ca0136f90bf606f55e7a72242ee9fd6ba78
johnmcraig <jmcraig32884@gmail.com>
2021-01-16 19:39:19 -0500

    Refactored GetById method to use stored procedure. Formatted the rest of the catch blocks on remaining methods that were inline

--- src/DataDashboard.Infrastructure/Data/CustomerNpgsqlRepository.cs
+++ src/DataDashboard.Infrastructure/Data/CustomerNpgsqlRepository.cs
@@ -21,19 +21,22 @@ namespace DataDashboard.Infrastructure.Data
 
         public async Task<Customer> GetByIdAsync(int id)
         {
-            const string query = "SELECT * FROM \"public\".\"Customers\" " +
-                                 " WHERE \"Id\" = @Id";
+            //const string query = "SELECT * FROM \"public\".\"Customers\" " +
+            //                     " WHERE \"Id\" = @Id";
             try
             {
                 var customer = await _dataAccess.LoadData<Customer, dynamic>
-                    (query, new
+                    ("dbo.spCustomer_GetById", new
                     {
                         @Id = id
-                    }, ConnectionString);
+                    }, ConnectionString, true);
 
                 return customer.FirstOrDefault();
             }
-            catch (Exception ex) { throw new Exception($"There was an error retrieving a record: {ex}"); }
+            catch (Exception ex) 
+            { 
+                throw new Exception($"There was an error retrieving a record: {ex}"); 
+            }
         }
 
         public async Task<IList<Customer>> FindBySearch(string search)
@@ -67,7 +70,10 @@ namespace DataDashboard.Infrastructure.Data
 
                 return customers.ToList();
             }
-            catch (Exception ex) { throw new Exception($"There was an error retrieving a record: {ex}"); }
+            catch (Exception ex) 
+            { 
+                throw new Exception($"There was an error retrieving a record: {ex}"); 
+            }
         }
 
         public async Task<IList<Customer>> ListAllWithPaging(int page, int pageSize)
@@ -88,7 +94,10 @@ namespace DataDashboard.Infrastructure.Data
 
                 return customers.ToList();
             }
-            catch (Exception ex) { throw new Exception($"There was an error retrieving a record: {ex}"); }
+            catch (Exception ex) 
+            { 
+                throw new Exception($"There was an error retrieving a record: {ex}"); 
+            }
         }
 
         public async Task<IList<Customer>> ListAllWithSearchingAndPaging(string search, int page, int pageSize)
@@ -110,7 +119,10 @@ namespace DataDashboard.Infrastructure.Data
 
                 return customers.ToList();
             }
-            catch (Exception ex) { throw new Exception($"There was an error retrieving a record: {ex}"); }
+            catch (Exception ex) 
+            { 
+                throw new Exception($"There was an error retrieving a record: {ex}"); 
+            }
         }
 
         public async Task<Customer> Create(Customer entity)
