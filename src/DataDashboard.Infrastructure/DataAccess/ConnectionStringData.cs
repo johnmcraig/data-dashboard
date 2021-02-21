e7bdf5733a4260834764d549988fbc7b14ebe140
johnmcraig <jmcraig32884@gmail.com>
2021-02-21 00:45:18 -0500

    Created a class to contain and set a string for appsettings connections

---
+++ src/DataDashboard.Infrastructure/DataAccess/ConnectionStringData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDashboard.Infrastructure.DataAccess
+{
+    public class ConnectionStringData
+    {
+        public string ConnectionString { get; set; } = "DefaultConnection";
+    }
+}
